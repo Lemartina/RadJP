076d34949f005cc8777a3733567a79ae60e42c97
Martina Novoselac <novoselac.martina@gmail.com>
2022-11-22 21:34:47 +0100

    Riješen zadatak 5

--- sql/zadatak5.sql
+++ sql/zadatak5.sql
@@ -1,5 +1,4 @@
-
-# C:\xampp\mysql\bin>mysql -uroot < C:\Users\Administrator\Documents\GitHub\EdunovaJP27\SQL\zadatak5.sql
+#  C:\xampp\mysql\bin\mysql -uroot < C:\Users\Administrator\Documents\GitHub\RadJP\RadJP\sql\zadatak5.sql
 
 drop database if exists muzej;
 create database muzej;
@@ -9,34 +8,35 @@ create table izlozba(
     sifra int not null primary key auto_increment,
     naziv varchar(50),
     vrijemedatum datetime,
-    sponzor int,
-    kustos int
+    kustos int not null,
+    sponzor int not null
 );
 
 create table djelo(
     sifra int not null primary key auto_increment,
-    nazivdj varchar(50),
+    nazivdj varchar(50) not null,
     autor varchar(50),
     vrijednost decimal(18,2),
-    izlozba int
+    izlozba int not null
+   
 );
-
-# vanjski ključ 
-alter table djelo add foreign key (izlozba) references izlozba(sifra);
-
-# nedostaje još vanjskih ključeva nije gotovo
 
 create table kustos(
     sifra int not null primary key auto_increment,
     iban char(35),
     oib char(11),
-    imekustos varchar(50),
-    prezimekustos varchar (50)
+    imekustos varchar(50) not null,
+    prezimekustos varchar (50) not null
 );
 
-create table sponzor(
+create  table sponzor(
     sifra int not null primary key auto_increment,
     brojugovora varchar (20),
-    imesponzora varchar(50),
-    prezimesponzora varchar (50)
+    imesponzora varchar(50) not null,
+    prezimesponzora varchar(50) not null
 );
+
+
+alter table izlozba add foreign key (kustos) references kustos(sifra);
+alter table izlozba add foreign key (sponzor) references sponzor(sifra);
+alter table djelo add foreign key (izlozba) references izlozba(sifra);
