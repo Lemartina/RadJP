68ed735520933699fbfbf27e490e62d9cbf8ab07
Martina Novoselac <novoselac.martina@gmail.com>
2022-11-25 23:45:05 +0100

    Potrebno još popuniti tablice

--- sql/zadatak7.sql
+++ sql/zadatak7.sql
@@ -1,14 +1,18 @@
 # c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Administrator\Documents\GitHub\RadJP\RadJP\sql\zadatak7.sql
 
-
+# Baza
 drop database if exists taxi;
 create database taxi default charset utf8;
 use taxi;
 
+
+# Tablice
 create table vozilo(
     sifra int not null primary key auto_increment,
-    redistracija varchar(10),
+    registracija varchar(10),
     maxmjesta char(2),
+    autosjedalica boolean,
+    invalid boolean,
     vozac int
 
 );
@@ -24,6 +28,7 @@ create table putnik(
     sifra int not null primary key auto_increment,
     dob char (2),
     lokacija varchar(20),
+    vrijemedolaska time,
     vozac int
 );
 
@@ -32,6 +37,30 @@ create table voznja(
     sifra int not null primary key auto_increment,
     trajanje time,
     cijena decimal(18,2),
-    vrijemedataum  datetime,
-    putnik int
+    vrijemedataum datetime,
+    putnik int, 
+    vozilo int,
+    vozac int
+    
 );
+
+# Vanjski ključevi
+
+alter table vozilo add foreign key ( vozac) references vozac(sifra);
+alter table putnik add foreign key (vozac) references vozac (sifra);
+alter table voznja add foreign key (putnik) references putnik (sifra);
+alter table voznja add foreign key (vozilo) references vozilo (sifra);
+alter table voznja add foreign key (vozac) references vozac (sifra);
+
+
+# Popunjavanje tablica
+
+insert into putnik (sifra, dob, lokacija, vrijemedolaska, vozac)
+values 
+(null, 'dijete', 'Svete Ane 17 Osijek', '18:00:00', '1'),
+(null, 'odrasla osoba', 'Ivana Mažurainića 17 Osijek', '2'),
+(null, 'dijete', 'Petra Preradovoća 3 Tenja', '2'),
+(null, 'dijete', 'A. Waldingera 45 Osijek', '3')
+;
+
+
