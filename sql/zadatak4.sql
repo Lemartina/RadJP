e95252fd75e846438aa44cc80a95b407528e072d
Martina Novoselac <novoselac.martina@gmail.com>
2022-11-27 02:06:30 +0100

    Popunjene tablice - gotovo

--- sql/zadatak4.sql
+++ sql/zadatak4.sql
@@ -46,3 +46,57 @@ alter table dijete add foreign key (odgajateljica) references odgajateljica(sifr
 alter table odgajateljica add foreign key (strucnasprema) references strucnasprema(sifra);
 
 
+#popunjavanje tablica
+
+insert  into strucnasprema (sifra, naziv)
+values
+(null, 'VSS'),
+(null, 'SSS'),
+(null, 'VŠS');
+
+
+
+insert into odgajateljica (sifra, imeodg, prezimeodg, oib, iban, strucnasprema)
+values
+(null, 'Iva', 'Ivić', 70359334978,'HR8424840081913429276', '1' ),
+(null, 'Petra', 'Pertić', 07928222303,'HR2624840088739258725', '1' ),
+(null, 'Marija', 'Marić', 49289914637,'HR3324840082999362945', '1' ),
+(null, 'Marko ', 'Marković', 67057213295,'HR6723600008379656892', '1' );
+
+insert into skupina (sifra, naziv, cijena, program)
+values
+( null, 'Bubamare', '500.00', 'Cjelodnevni'),
+(null, 'Ribice', '600.00', 'Cjelodenvni'),
+(null, 'Žabice', '550.00', 'Cjelodnevni'),
+(null, 'Ježići', '450', ' Cjelodnevni');
+insert into dijete(sifra, ime, prezime, brojugovora, skupina, odgajateljica)
+values
+(null, 'Luka', 'Ivić', '22/001', 1, 1),
+(null, 'Luka', 'Ivić', '22/002', 1, 1),
+(null, 'Luka', 'Ivić', '22/003', 2, 3),
+(null, 'Luka', 'Ivić', '22/004', 2, 3),
+(null, 'Luka', 'Ivić', '22/005', 3, 4),
+(null, 'Luka', 'Ivić', '22/006', 3, 4),
+(null, 'Luka', 'Ivić', '22/007', 4, 2),
+(null, 'Luka', 'Ivić', '22/008', 4, 2),
+(null, 'Luka', 'Ivić', '22/009', 4, 2),
+(null, 'Luka', 'Ivić', '22/010', 4, 2);
+
+update dijete set ime='Petar' where sifra=2;
+update dijete set ime='Ivor' where sifra=3;
+update dijete set ime='Ana' where sifra=4;
+update dijete set ime='Jurica' where sifra=5;
+update dijete set ime='Ivana' where sifra=6;
+update dijete set ime='Maja' where sifra=7;
+update dijete set ime='Sara' where sifra=8;
+update dijete set ime='Iris' where sifra=9;
+
+update dijete set prezime='Marković' where sifra=2;
+update dijete set prezime='Horvat' where sifra=3;
+update dijete set prezime='Jurić' where sifra=4;
+update dijete set prezime='Tekvavčić' where sifra=5;
+update dijete set prezime='Majiž' where sifra=6;
+update dijete set prezime='Norković' where sifra=7;
+update dijete set prezime='Jedvaj' where sifra=8;
+update dijete set prezime='Dodig' where sifra=9;
+update dijete set prezime='Horvat' where sifra=10;
