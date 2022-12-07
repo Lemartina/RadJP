c2162082729a5a3aa94e1a15627ac832d4e70b97
Martina Novoselac <novoselac.martina@gmail.com>
2022-12-07 06:28:12 +0100

    u radu

--- Kolokvij/vjezba4.sql
+++ Kolokvij/vjezba4.sql
@@ -84,7 +84,51 @@ alter table zena_mladic add foreign key(mladic) references mladic(sifra);
 # unos u tablice
 
 
+insert into ostavljen(sifra, modelnaocala, introvertno, kuna)
+values
+(null, 'maxmara', 1, 10.53),
+(null, 'rayban', 0, 26.53),
+(null, 'maxmara', 0 53.53);
+
 insert into punac(sifra, treciputa, majica, jmbag, novcic, maraka, ostavljen)
-values (null, 'zelena', '1234566666', 10, 20,1),
+values 
+(null, 'zelena', '1234566666', 10, 20,1),
 (null, 'plava', '1234566666', 10, 20,1),
-(null, 'žuta', '1234566666', 10, 20,1),
+(null, 'žuta', '1234566666', 10, 20,1);
+
+insert into mladic(sifra, kuna, lipa, nausnica, stilfrizura, vesta)
+values
+(null, 11,63, 1, 'poluduga kosa', 'crvena'),
+(null, 12,70, 1, 'kratka kosa', 'plava'),
+(null, 13,69, 1, 'kratka kosa', 'zelena');
+
+insert into zena (sifra, suknja, lipa, prstena)
+values
+(null,'poluduga ',105, 0),
+(null,'kratka ',15, 1),
+(null,'duga ',1654, 0);
+
+
+insert into mladic(sifra, zena, mladic)
+values 
+(null, 1, 1),
+(null, 2, 2),
+(null, 3, 3);
+
+insert into snasa (sifra, introvertno, treciputa, haljina, zena)
+values
+(null, 0, '2022-01-05 00:00:00' , 'ljubičasta', 1),
+(null, 0, '2022-02-05 00:00:00' , 'crvena', 2),
+(null, 0, '2022-03-05 00:00:00' , 'plava', 3);
+
+insert into becar(sifra, novcica, kratkamajica, bojaociju, snasa)
+values
+(null, 223.56, 'pamučna','plava',1),
+(null, 236.56, 'poliester','crvena',2),
+(null, 159.56, 'lan','plava',3);
+
+insert into prijatelj (sifra, eura, prestena, gustoca, jmbag, suknja, becar)
+values
+(null, 32.56, 2, 464.5465, '12345678978', 1),
+(null, 598.56, 2, 345.5465, '94545664548', 2),
+(null, 77896.56, 2, 545.5465, '12344544978', 3);
