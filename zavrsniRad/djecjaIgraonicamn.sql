752a3e29536be12fe915e552024f32a284e38186
Martina Novoselac <novoselac.martina@gmail.com>
2022-12-09 23:51:48 +0100

    Popunejne tablice

--- zavrsniRad/djecjaIgraonicamn.sql
+++ zavrsniRad/djecjaIgraonicamn.sql
@@ -12,7 +12,7 @@ create table usluga(
     sifra int not null primary key auto_increment,
     naziv varchar (50),
     cijena decimal(18,2),
-    jeidnicaMjere int not null,
+    jeidnicaMjere varchar(10),
     kolicina decimal(18,2)
 );
 
@@ -22,7 +22,7 @@ create table dijete(
     ime varchar(20),
     prezime varchar(20), 
     imeRoditelja varchar(20),
-    telefonRoditelja varchar (20)
+    telefonRoditelja varchar(20)
 
     
 );
@@ -68,9 +68,50 @@ alter table posjetaUsluga add foreign key (usluga) references usluga(sifra);
 
 # koje dijete koristi koju uslugu
 
-select ime.a, prezime.a, naziv.e
+select a.ime, a.prezime, e.naziv
 from dijete a 
 inner join posjetadijete b on a.sifra=b.dijete 
 inner join posjeta c on b.posjeta=c.sifra 
 inner join posjetausluga d on c.sifra=d.posjeta 
 inner join usluga e on d.usluga=e.sifra;
+
+# popunajanje tablica 
+
+insert into dijete (sifra, ime, prezime, imeRoditelja, telefonRoditelja)
+values
+(null, 'Marko', 'Marković', 'Ivana', '091/236-5236'),
+(null, 'Luka', 'Ivanković', 'Marija', '097/125-5246'),
+(null, 'Josipa', 'Josipović', 'Gabrijel', '098/259-3265');
+
+
+insert into djelatnik (sifra, ime, prezime, oib, iban, radnoMjesto)
+values
+(null, 'Ivana', 'Ivković', 93197060481, 'HR7325000098193449454', 'animator' ),
+(null, 'Marina', 'Marković', 26403646008, 'HR5723400095351463692', 'teta čuvalica' ),
+(null, 'Petar', 'Marinić', 81890337429, 'HR3923600008751454128', 'animator' );
+
+insert into usluga(sifra, naziv, cijena, jeidnicaMjere, kolicina)
+values
+(null, 'Rođendan', '750.00', 'sat', 5),
+(null, 'Dnevno čuvanje', '250.00', 'sat', 1),
+(null, 'Tiskanje pozivnica', '10.00', 'kom', 1),
+(null, 'Torta', '500.00', 'kom', 1),
+(null, 'Umjetnička radionica', '100.00', 'sat', 2);
+
+insert into posjeta(sifra, datumVrijemeDolaska, datumVrijemeOdlaska, djelatnik)
+values
+(null, '2022-05-01 13:00:00', '2022-05-01 14:00:00',2),
+(null, '2022-06-01 14:00:00', '2022-05-01 19:00:00',1),
+(null, '2022-07-01 09:00:00', '2022-05-01 11:00:00',3);
+
+insert into posjetadijete (posjeta, dijete)
+values
+(1,1),
+(2,2),
+(3,3);
+
+insert into posjetausluga (posjeta, usluga)
+values
+(1,2),
+(2,1),
+(3,5);
