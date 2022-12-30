3a1b1e6957327d7a04ae67b7928374fe51968ffc
Martina Novoselac <novoselac.martina@gmail.com>
2022-12-30 23:55:23 +0100

    riješen zadatak Z07

---
+++ java/eclipse/Edunova03/src/edunova/Z07.java
@@ -0,0 +1,26 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z07 {
+
+	public static void main(String[] args) {
+		// Program unosi 2 broja
+		// Ako su oba broja parni
+		// ispisuje njihov zbroj
+		// inače ispisuje njihovu razliku
+
+		
+		
+		int a=Integer.parseInt(JOptionPane.showInputDialog("Upiši jedan broj"));
+		int b= Integer.parseInt(JOptionPane.showInputDialog("Upiši drugi broj"));
+		
+		
+		if ((a%2==0) && (b%2==0))
+		{System.out.println(a+b);
+		}else {
+			System.out.println(a-b);
+		}
+	}
+
+}
