503be68b5cae65a94fbb7d82a0d4b45830044e85
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-01 15:23:53 +0100

    ispis ukupno

---
+++ java/eclipse/Edunova100/src/edunova/ZZ01.java
@@ -0,0 +1,20 @@
+package edunova;
+
+public class ZZ01 {
+
+	public static void main(String[] args) {
+		// Kreirati program koji unosi 24 broja, ispisuje njihov zbroj, najmanji i  najveći uneseni broj.
+		
+		
+		int ukupno=0;
+		Math.max(ukupno, ukupno);
+		
+		for(int m=1;m<=24;m++) {
+			ukupno=ukupno+m;
+		}
+System.out.println("Zbroj prva 24 broja je: " + ukupno);
+
+System.out.println(Math.max(m,ukupno));
+	}
+
+}
