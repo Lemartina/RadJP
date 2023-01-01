b1e021a53b4e901ee08d8192de8a15e571c1d474
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-01 16:53:05 +0100

    napravljena tablica 15*15 bez potpisivanja

---
+++ java/eclipse/Edunova100/src/edunova/ZZ02.java
@@ -0,0 +1,24 @@
+package edunova;
+
+public class ZZ02 {
+
+	public static void main(String[] args) {
+		// Kreirati program koji ispisuje tablicu množenja 15x15
+		
+	
+		
+		
+		for(int i=1; i<=15;i++) {
+			for(int j=1;j<=15;j++) {
+						System.out.print(" "+ i*j);
+			}
+			
+			
+			System.out.println();
+		}
+	}
+	}
+	
+	
+
+
