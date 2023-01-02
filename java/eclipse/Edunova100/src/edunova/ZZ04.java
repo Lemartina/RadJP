ae3ca8dac2a7d7b01710d46b1e7eaeff138afdbe
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-02 22:48:01 +0100

    kreiranje matrica unosom stupaca i redaka

--- java/eclipse/Edunova100/src/edunova/ZZ04.java
+++ java/eclipse/Edunova100/src/edunova/ZZ04.java
@@ -1,5 +1,7 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class ZZ04 {
 
 	public static void main(String[] args) {
@@ -7,30 +9,38 @@ public class ZZ04 {
 		//matrice 4x4 te ispisuje njihov zbroj
 		
 		//1. matrica 4*4
-		for(int i=1; i<=4;i++) {
-			for(int j=1;j<=4;j++) {
-					System.out.print(" "+ i*j);
-			}
-						
-			System.out.println();
-
-	}
-		System.out.println("\t");
 		
+		System.out.println("1. matrica");
+		System.out.println("\t");
+		//unos podataka
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka za 1. matricu:"));
+		int j= Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca za 1. matricu:"));
+		
+		
+		// foriranje martice i ispis
+		for(i=1; i<=4;i++) {
+			for (j=1;j<=4;j++) {
+			System.out.print(" "+ i*j);
+			} 					
+			System.out.println();
+		}
+		System.out.println("\t");
 		//2. matrica 4*4
-		for(int a=1; a<=4;a++) {
-			for(int b=1;b<=4;b++) {
+		
+		// unos podataka
+		
+		int a= Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka za 2. matricu:"));
+		int b= Integer.parseInt(JOptionPane.showInputDialog("Unesi broj sutupaca za 2. marticu"));
+		
+		System.out.println("2. matrica");
+		System.out.println("\t");
+		for(a=1; a<=4;a++) {
+			for(b=1;b<=4;b++) {
 					System.out.print(" "+ a*b);
 			}
 						
 			System.out.println();
 
 }
-		
-		
-		//zbroj matrica
-		
-		System.out.println((i*j)* (a*b));
-
-	}
-	}
+		System.out.println("\t");
+}}
