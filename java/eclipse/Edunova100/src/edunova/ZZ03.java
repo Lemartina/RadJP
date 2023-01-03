22c9dd52325d231953716c511e9d2de43c179a54
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-03 23:26:05 +0100

    dodavanje ispisa brojeva

--- java/eclipse/Edunova100/src/edunova/ZZ03.java
+++ java/eclipse/Edunova100/src/edunova/ZZ03.java
@@ -6,34 +6,33 @@ public class ZZ03 {
 
 	public static void main(String[] args) {
 		// Kreirati program koji za dva
-		//unesena broja ispisuje sve prim (djeljivi sami sobom i s 1)
-		//brojeve između njih.
+		// unesena broja ispisuje sve prim (djeljivi sami sobom i s 1)
+		// brojeve između njih.
 
-		
-		int jedan=Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
-		int dva=Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+		int jedan = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int dva = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
 
-		System.out.println(jedan);
-		System.out.println(dva);
-		
-		int rezultat=0;
-		for (int i=jedan; i<dva; i++) {
-				
-			
-			if (i/1==1)  {
-				rezultat = rezultat+i;}
-				System.out.println(rezultat);
+		System.out.println("Ovo je prvi broj: " + jedan);
+		System.out.println("Ovo je drugi broj: " + dva);
+
+		int min = jedan < dva ? jedan : dva;
+		int max = jedan > dva ? jedan : dva;
+
+		int rezultat = 0;
+		for (int i = min; i < max; i++) {
+			if (i / 1 == 1) {
+				rezultat+=i;{
 			}
-			 else if (i/i==0) {
-				 rezultat = rezultat+i;}
-				System.out.println(rezultat);
-				}else
+				System.out.println("Ovo je rezezultat1: "+ rezultat);
+			} else if (i / i == 0) {
+				System.out.println("Ovo je rezultat2: " + rezultat);
+				{
+					rezultat += rezultat + i;
+				}
 
-	{
-		System.out.println("Nema prim brojeva između dva unesena broja");
+			}
+
+		}
+
 	}
-}
-
-}}
-
 }
