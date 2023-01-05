f742a386775fe046ff4d1bafedbc648f0764282a
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-05 22:30:40 +0100

    gotovo

--- java/eclipse/Edunova100/src/edunova/ZZ02.java
+++ java/eclipse/Edunova100/src/edunova/ZZ02.java
@@ -4,25 +4,21 @@ public class ZZ02 {
 
 	public static void main(String[] args) {
 		// Kreirati program koji ispisuje tablicu množenja 15x15
-
+		
+		System.out.print("Tablica množenja 15*15");
+		System.out.print("\n \n");
 		for (int i = 1; i <= 15; i++) {
 			for (int j = 1; j <= 15; j++) {
-//			 		if (j==1) {
-//					System.out.print("  U " + i * j);
-//				
-//				}
 
 				if ((i * j >= 1) && (i * j <= 9)) {
-					System.out.print("     " + i * j);
-				} else if ((i * j > 9) && (i * 1 <= 99)) {
-					System.out.print("    " + i * j);
-				} else if ((i>=100)&&(i<=225)) {
-					System.out.println("   "+i*j);
+					System.out.print("---" + i * j);
+				} else if ((i * j >= 10) && (i * j <= 99)) {
+					System.out.print("--" + i * j);
+				} else {
+					System.out.print("-" + i * j);
 				}
 			}
-//			if (i==15) {
-//				System.out.println();
-//			}
+
 			System.out.println();
 		}
 	}
