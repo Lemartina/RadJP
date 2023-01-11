1bb5c7e390046ea104b2b9080f2b1b860318b1b4
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-11 07:17:08 +0100

    z02 gotov

---
+++ java/eclipse/Edunova05/src/edunova/Z02.java
@@ -0,0 +1,24 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z02 {
+
+	public static void main(String[] args) {
+		// Korisnik unosi 5 brojeva
+		// između 2 i 4
+		// Program ispisuje njihov umnožak
+		long umnozak = 1;
+		int brojac = 1;
+		int broj;
+		while (brojac < 5) {
+			broj = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj između 2 i 4"));
+			if (broj >= 2 && broj <= 4) {
+				umnozak *= broj;
+				brojac++;
+			}
+		}
+		System.out.println(umnozak);
+	}
+
+}
