6f1da00dc38e319e1a3651d2c550a12cfd211019
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-12 22:44:56 +0100

    rješavanje zadatka

--- java/eclipse/Edunova100/src/edunova/ZimskiZadaci6.java
+++ java/eclipse/Edunova100/src/edunova/ZimskiZadaci6.java
@@ -9,30 +9,76 @@ public class ZimskiZadaci6 {
 //Kreirati program koji ispisuje cikličnu matricu 
 
 		// unos podataka
-
-		int red = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka:"));
 		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj sutupaca:"));
-
-		int redPocetak = 1;
-		int redKraj = red - 1;
-		int stupacPocetak = 1;
-		int stupacKraj = stupac - 1;
-		int broj = 1;
-		int i;
-
-
+		int red = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka:"));
+		
 
 		int Matrica[][] = new int[stupac][red];
+		int broj = 1;
+		int stupacPocetak = 0;
+		int stupacKraj = stupac - 1;
+		int redPocetak = 0;
+		int redKraj = red - 1;
+		
+		
+		int i;
+		int j;
+
+//1
 
 		vanjska: 
-			for (;;){
+			for (i=1; broj<= stupac * red;) {
 			for (i = redKraj; i >= redPocetak; i--) {
-				if (broj>= stupac * red) {
+				if (broj > stupac * red) {
 					break vanjska;
 				} else {
 					Matrica[stupacKraj][i] = broj++;
 				}
+
+			}
+			stupacKraj--;
+//2
+			for (i = stupacKraj; i >= stupacPocetak; i--) {
+				if (broj > stupac * red) {
+					break vanjska;
+				} else {
+					Matrica[i][redPocetak] = broj++;
+				}
+
+			}
+			stupacKraj++;
+//3
+			for (i = redPocetak; i <= redKraj; i++) {
+
+				if (broj > stupac * red) {
+					break vanjska;
+				} else {
+					Matrica[stupacPocetak][i] = broj++;
+
+				}
+			}
+			stupacPocetak++;
+
+			
+			//4
+			for (i = stupacPocetak; i <= stupacKraj; i++) {
+
+				if (broj > stupac * red) {
+					break vanjska;
+				} else {
+					Matrica[i][redKraj] = broj++;
+
+				}
+			}
+			redKraj--;
+
+			for (j = 0; j < stupac; j++) {
+				for (i = 0; i < red; i++) {
+					System.out.print(Matrica[j][i]+" ");
+				}
 				System.out.println();
 			}
-	}stupacKraj--;
-}}
+
+		}
+	}
+}
