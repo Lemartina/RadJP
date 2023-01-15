8bc105b3dcc5b5d7a93a8a55e6990ace38515a8f
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-15 16:57:41 +0100

    priprema za zadatak - upute

---
+++ java/eclipse/Edunova06/src/edunova/Z06.java
@@ -0,0 +1,14 @@
+package edunova;
+
+public class Z06 {
+	
+	//upute:
+		//string ima char
+		//znakovi se stavlja u niz
+		//poziva istu metodu dok ne dođe do broja manjeg od 100
+		// koristiti i petlju i uvjetno grananje
+		
+
+	
+	
+}
