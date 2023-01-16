c96992443777b6e54ffeb3aa4e451de9e376215a
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-16 23:16:56 +0100

    početak

--- java/eclipse/Edunova06/src/edunova/Z06.java
+++ java/eclipse/Edunova06/src/edunova/Z06.java
@@ -8,7 +8,39 @@ public class Z06 {
 		//poziva istu metodu dok ne dođe do broja manjeg od 100
 		// koristiti i petlju i uvjetno grananje
 		
-
 	
+//	spjiiti 2 stringa u 1
+//
+//	koristiti for petlju
+//
+//	vrti string broji a
+//
+//	vrti strin broji b
+//
+//	dobiješ niz
+//
+//	niz metoda rekurzivno novi = novi niz
+//
+//	ako je parni broj elenemana/2
+//
+//	/2 +1 neprarni
+//
+//	<100 vrati rezultat int
+
+public static void main(String[] args) {
+	String ime1= "Marta";
+	String ime2= "Manuel";
+	
+	String ljubav= ime1 + ime2;
+	System.out.println(ljubav);
+	
+	int zbroj=0;
+	for (char i =0; i<=ljubav.length(); i++)
+	{
+		zbroj += zbroj + i;
+		char ch= ljubav.charAt(i);
+	
+	}
+}
 	
 }
