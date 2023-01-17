7fdb4186738c980016bb0eaae10f799d301cffb5
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-17 23:20:33 +0100

    for petlju treba popraviti

--- java/eclipse/Edunova06/src/edunova/Z06.java
+++ java/eclipse/Edunova06/src/edunova/Z06.java
@@ -1,14 +1,13 @@
 package edunova;
 
 public class Z06 {
-	
-	//upute:
-		//string ima char
-		//znakovi se stavlja u niz
-		//poziva istu metodu dok ne dođe do broja manjeg od 100
-		// koristiti i petlju i uvjetno grananje
-		
-	
+
+	// upute:
+	// string ima char
+	// znakovi se stavlja u niz
+	// poziva istu metodu dok ne dođe do broja manjeg od 100
+	// koristiti i petlju i uvjetno grananje
+
 //	spjiiti 2 stringa u 1
 //
 //	koristiti for petlju
@@ -27,20 +26,27 @@ public class Z06 {
 //
 //	<100 vrati rezultat int
 
-public static void main(String[] args) {
-	String ime1= "Marta";
-	String ime2= "Manuel";
-	
-	String ljubav= ime1 + ime2;
-	System.out.println(ljubav);
-	
-	int zbroj=0;
-	for (char i =0; i<=ljubav.length(); i++)
-	{
-		zbroj += zbroj + i;
-		char ch= ljubav.charAt(i);
-	
-	}
-}
-	
-}
+//	https://www.youtube.com/watch?v=mrXDivW-vP4
+//	https://www.youtube.com/watch?v=iHCon5VmSyk
+
+	public static void main(String[] args) {
+		String ime1 = "Marta";
+		String ime2 = "Manuel";
+		String slovo;
+
+		String ljubav = ime1 + ime2;
+		System.out.print(ljubav);
+		int i, j;
+		slovo = "A";
+
+		int zbroj = 0;
+		for (i = 0; i <= ljubav.length(); i++) {
+			for (j = 0; j <= ljubav.length(); j++) {
+//		if (ljubav.charAt(i)== slovo) {
+		
+
+				zbroj = zbroj + 1;
+
+			System.out.print( i * j);
+		}
+		}}}
