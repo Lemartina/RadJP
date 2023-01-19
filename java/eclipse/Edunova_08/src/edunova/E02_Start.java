a0b8e7c67b7224fa1dba0626a9d1d105a3246872
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-20 00:11:23 +0100

    ponvaljanje

--- java/eclipse/Edunova_08/src/edunova/E02_Start.java
+++ java/eclipse/Edunova_08/src/edunova/E02_Start.java
@@ -11,8 +11,29 @@ public static void main(String[] args) {
 	Osoba osoba  = new Osoba();
 	// klasa će sakriti svoja svojstva i učiniti ih dostupnima putem metoda -> getteri i setteri
 
+osoba.setIme("Pero");
+osoba.setPrezime("Perić");
+osoba.setGodine(24);
 
-	
+System.out.println(osoba.getPrezime());
+
+Osoba  [] osobe = new Osoba [2];
+osobe[0]=osoba;
+
+
+osoba = new Osoba();
+osoba.setIme("Marita");
+osobe[1]=osoba;
+
+for (Osoba o: osobe) {
+	System.out.println(o.getIme());
+}
 	
 }
+//private static Osoba kreirajOsobu(String ime, String prezime, int godine) {
+//Osoba osoba= new Osoba();
+//osoba.setGodine(godine);
+//osoba.setIme(prezime);
+//osoba.setPrezime();
+//return osoba;
 }
