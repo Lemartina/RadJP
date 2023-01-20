9fed8c79a329860143d7bbe191c4cc4d92ad97c4
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-20 23:58:08 +0100

    ponavljanje

--- java/eclipse/Edunova_08/src/edunova/E01_Problem.java
+++ java/eclipse/Edunova_08/src/edunova/E01_Problem.java
@@ -1,6 +1,17 @@
 package edunova;
 
 public class E01_Problem {
-	
 
+	// String ime="Pero"; ovo ne valja jer imamo
+	// samo jedan podatak u varijabli nama teba više podataka, više imena i to se ne
+	// može na ovaj način
+	// problem je kada želim upisati više vrijdednosti
+	// varijabla je mehanizam za pohranu vrijednosti
+	// da bi mogli pohratiti više vrijednosti i
+	// objediniti ih u objekt potrebno je prvo napraviti klasu
+// klasa je opisnik objekta --Osoba
+	//objekt osoba ima svoja svojstva kao u tablici u sql-u
+	
+	// varijabla je za primitivne tipove
+	//objekt je za tipove koji nisu primitivni
 }
--- java/eclipse/Edunova_08/src/edunova/E02_Start.java
+++ java/eclipse/Edunova_08/src/edunova/E02_Start.java
@@ -1,39 +1,53 @@
 package edunova;
 
 public class E02_Start {
-public static void main(String[] args) {
-	
-	//tip podatka može biti bilo koja klasa koju smo definirali
-	//Osoba je tip podataka - klada
-	//osobe je instanca klase - objekt - vraijabla
-	//uobijčajeno je da se objekt zove kao klasa razlik je u malo/veliko slovo
-	// new poziv konstruktora klase
-	Osoba osoba  = new Osoba();
-	// klasa će sakriti svoja svojstva i učiniti ih dostupnima putem metoda -> getteri i setteri
+	public static void main(String[] args) {
 
-osoba.setIme("Pero");
-osoba.setPrezime("Perić");
-osoba.setGodine(24);
+		// tip podatka može biti bilo koja klasa koju smo definirali
+		// Osoba je tip podataka - klasa
+		// osoba je instanca klase - objekt - vraijabla
+		// uobijčajeno je da se objekt zove kao klasa razlika je u malo/veliko slovo
+		// new poziv konstruktora klase
+		Osoba osoba = new Osoba();
+		// klasa će sakriti svoja svojstva i učiniti ih dostupnima putem metoda ->
+		// getteri i setteri
+		// OOP
+		// principi:
+//		1. klasa-objekt
+//		2. učahurivanje
+//		3. nasljeđivanje
+//		4. generici
+//		5. polimorfizam
+//		6. sučelja
+//		7. anutacije
 
-System.out.println(osoba.getPrezime());
+		// postavljanje vrijednosti svojstava pomoću getera i setera
+		osoba.setIme("Pero");
+		osoba.setPrezime("Perić");
+		osoba.setGodine(24);
 
-Osoba  [] osobe = new Osoba [2];
-osobe[0]=osoba;
+		System.out.println(osoba.getPrezime());
 
+		Osoba[] osobe = new Osoba[2];
+		osobe[0] = osoba;
 
-osoba = new Osoba();
-osoba.setIme("Marita");
-osobe[1]=osoba;
+		osoba = new Osoba();
+		osoba.setIme("Marina");
+		osobe[1] = osoba;
+		osobe[2] = kreirajOsobu("Ana", "Gal", 19);
+		osobe[3]= kreirajOsobu("Tin", "Ked", 19); 
 
-for (Osoba o: osobe) {
-	System.out.println(o.getIme());
-}
-	
-}
-//private static Osoba kreirajOsobu(String ime, String prezime, int godine) {
-//Osoba osoba= new Osoba();
-//osoba.setGodine(godine);
-//osoba.setIme(prezime);
-//osoba.setPrezime();
-//return osoba;
+		for (Osoba o : osobe) {
+			System.out.println(o.getIme());
+		}
+
+	}
+
+	private static Osoba kreirajOsobu(String ime, String prezime, int godine) {
+		Osoba osoba = new Osoba();
+		osoba.setGodine(godine);
+		osoba.setIme(ime);
+		osoba.setPrezime(prezime);
+		return osoba;
+	}
 }
