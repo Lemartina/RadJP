7c1607c1fbdb7857af8f7ccf50b945a02f49ac7f
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-28 16:05:35 +0100

    geteri, seteri, konstruktori

---
+++ java/eclipse/ZavrsniRad/src/zavrsniRad/Entitet.java
@@ -0,0 +1,28 @@
+package zavrsniRad;
+
+abstract public class Entitet {
+	private int sifra;
+
+	
+	
+	public Entitet() {
+		super();
+	}
+
+	public Entitet(int sifra) {
+		super();
+		this.sifra = sifra;
+	}
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	
+	
+	
+
+}
