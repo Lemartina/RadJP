b6d361ba3f70323b004be8eff3038a10aa6c04e1
Martina Novoselac <novoselac.martina@gmail.com>
2023-01-31 05:59:25 +0100

    ponavljanje

---
+++ java/eclipse/Edunova11/src/edunova/Pomocno.java
@@ -0,0 +1,11 @@
+package edunova;
+
+import java.util.Scanner;
+
+public class Pomocno {
+	
+	public static Scanner ulaz;
+	
+
+
+}
---
+++ java/eclipse/Edunova11/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+module edunova11 {
+	requires java.desktop;
+}
