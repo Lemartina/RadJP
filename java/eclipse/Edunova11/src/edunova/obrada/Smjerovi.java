a7fba1144a5e6cd82fdcf6f90066214f96a9627a
Martina Novoselac <novoselac.martina@gmail.com>
2023-02-02 23:27:39 +0100

    dodani geteri, seteri, konstruktori

--- java/eclipse/Edunova11/src/edunova/obrada/Smjerovi.java
+++ java/eclipse/Edunova11/src/edunova/obrada/Smjerovi.java
@@ -3,8 +3,8 @@ package edunova.obrada;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
+import edunova.Pomocno;
+import edunova.Start;
 import edunova.model.Smjer;
 
 public class Smjerovi {
@@ -18,6 +18,30 @@ public class Smjerovi {
 		super();
 		smjerovi = new ArrayList<>();
 	}
+	
+	
+	
+	public void izbornik() {
+		System.out.println("");
+		System.out.println("Smjer izbornik");
+		System.out.println("Dostupne opcije");
+		System.out.println("1. Pregled svih smjerova");
+		System.out.println("2. Unos novog smjera");
+		System.out.println("3. Promjena postojećeg smjera");
+		System.out.println("4. Brisanje smjera");
+		System.out.println("5. Povratak na glavni izbornik");
+		odabirIzbornika();
+	}
+	
+	private void odabirIzbornika() {
+		switch(Pomocno.unosBrojRaspon("Odaberi opciju: ", 1, 5)) {
+		case 5:
+			Start.glavniIzbornik();
+		}
+		
+	}
+
+	
 
 	public Smjerovi(List<Smjer> smjerovi) {
 		super();
