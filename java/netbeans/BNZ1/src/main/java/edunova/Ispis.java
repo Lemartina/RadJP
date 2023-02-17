a038cc42e0fbc4790bf30974ae3a9333872d2f8f
Martina Novoselac <novoselac.martina@gmail.com>
2023-02-17 23:50:35 +0100

    radi

--- java/netbeans/BNZ1/src/main/java/edunova/Ispis.java
+++ java/netbeans/BNZ1/src/main/java/edunova/Ispis.java
@@ -26,16 +26,26 @@ public class Ispis extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Ime = new javax.swing.JTextField();
+        txtIme = new javax.swing.JTextField();
         txtPrezime = new javax.swing.JTextField();
         btnPotvrdiUnos = new javax.swing.JButton();
         lblIspisImePezime = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        Ime.setText("Ime");
+        txtIme.setText("Ime");
+        txtIme.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtImeActionPerformed(evt);
+            }
+        });
 
         txtPrezime.setText("Prezime");
+        txtPrezime.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPrezimeActionPerformed(evt);
+            }
+        });
 
         btnPotvrdiUnos.setText("Potvrdi unos");
         btnPotvrdiUnos.addActionListener(new java.awt.event.ActionListener() {
@@ -53,7 +63,7 @@ public class Ispis extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(lblIspisImePezime, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(Ime)
+                        .addComponent(txtIme)
                         .addComponent(txtPrezime)
                         .addComponent(btnPotvrdiUnos, javax.swing.GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)))
                 .addContainerGap(163, Short.MAX_VALUE))
@@ -62,7 +72,7 @@ public class Ispis extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(36, 36, 36)
-                .addComponent(Ime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtIme, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(txtPrezime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -76,48 +86,27 @@ public class Ispis extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnPotvrdiUnosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPotvrdiUnosActionPerformed
-        // TODO add your handling code here:
+         lblIspisImePezime.setText(txtIme.getText()+" "+ txtPrezime.getText());
     }//GEN-LAST:event_btnPotvrdiUnosActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ispis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ispis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ispis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ispis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+     private void lblIspisImePrezimeAncestorAdded(javax.swing.event.AncestorEvent evt) {                                       
+      lblIspisImePezime.setText(txtIme.getText()+" "+ txtPrezime.getText());
+     }
+     
+    private void txtImeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtImeActionPerformed
+       
+    }//GEN-LAST:event_txtImeActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Ispis().setVisible(true);
-            }
-        });
-    }
+    private void txtPrezimeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPrezimeActionPerformed
+        
+    }//GEN-LAST:event_txtPrezimeActionPerformed
+
+  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField Ime;
     private javax.swing.JButton btnPotvrdiUnos;
     private javax.swing.JLabel lblIspisImePezime;
+    private javax.swing.JTextField txtIme;
     private javax.swing.JTextField txtPrezime;
     // End of variables declaration//GEN-END:variables
 }
