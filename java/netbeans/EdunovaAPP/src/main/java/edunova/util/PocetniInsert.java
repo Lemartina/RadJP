16d716db21aab3f29203a8eac5509ae06044dcc5
Martina Novoselac <novoselac.martina@gmail.com>
2023-02-26 00:41:05 +0100

    brisanje osobe i djelatnika

--- java/netbeans/EdunovaAPP/src/main/java/edunova/util/PocetniInsert.java
+++ java/netbeans/EdunovaAPP/src/main/java/edunova/util/PocetniInsert.java
@@ -5,7 +5,7 @@
 package edunova.util;
 
 import com.github.javafaker.Faker;
-import edunova.model.Osoba;
+
 import java.math.BigDecimal;
 import org.hibernate.Session;
 
@@ -16,24 +16,7 @@ import org.hibernate.Session;
 public class PocetniInsert {
 
     public static void izvedi() {
-        Session session = HibernateUtil.getSession();
-        Faker faker = new Faker();
-
-        Osoba o;
-        session.beginTransaction();
-        for (int i = 0; i < 100; i++) {
-            o = new Osoba();
-            o.setIme(faker.name().firstName());
-            o.setPrezime(faker.name().lastName());
-            o.setPrimanja(new BigDecimal(faker.number().randomNumber()));
-            o.setDatumRodenja(faker.date().birthday());
-            o.setAktivan(faker.bool().bool());
-
-            session.persist(o);
-
-        }
-
-        session.getTransaction().commit();
+      
 
     }
 }
